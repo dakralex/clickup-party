b0480a4c7fcaefccfd26961ebe07f7b19c24031c
Daniel Kral <dakral@protonmail.com>
2022-04-25 20:48:51 +0200

    Fix identation in ServiceProvider

--- src/Laravel/ClickUpPartyServiceProvider.php
+++ src/Laravel/ClickUpPartyServiceProvider.php
@@ -4,30 +4,29 @@ namespace Dakralex\ClickUpParty\Laravel;
 
 use Illuminate\Support\ServiceProvider;
 
-class ClickUpPartyServiceProvider extends ServiceProvider {
+class ClickUpPartyServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/clickup-party.php' => config_path('clickup-party.php'),
+            ], 'config');
+        }
+    }
 
-	/**
-	 * Boot the service provider.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		if ($this->app->runningInConsole()) {
-			$this->publishes([
-				__DIR__ . '/../config/clickup-party.php' => config_path('clickup-party.php'),
-			], 'config');
-		}
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->mergeConfigFrom(__DIR__ . '/../config/clickup-party.php', 'clickup-party');
-	}
-
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/clickup-party.php', 'clickup-party');
+    }
 }
